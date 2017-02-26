0c0215011553c2c6cde2f1e61eb0521a4d86b705
Vlad Veselov <vlad.veselov@gmail.com>
2017-02-26 17:06:59 +0200

    Reset three files to mainline/develop state to ensure they are unchanged (Windows line breaks?)

--- dev/tests/static/framework/Magento/TestFramework/Dependency/VirtualType/VirtualTypeMapper.php
+++ dev/tests/static/framework/Magento/TestFramework/Dependency/VirtualType/VirtualTypeMapper.php
@@ -47,6 +47,7 @@ class VirtualTypeMapper
             array_unshift($scopes, $scope);
         }
         $scopes[] = self::$mainScope;
+
         foreach ($scopes as $scp) {
             if (isset($this->map[$scp][$name])) {
                 return $this->map[$scp][$name];
@@ -90,8 +91,9 @@ class VirtualTypeMapper
             $scope = $this->getScopeFromFile($file);
             $doc = new DOMDocument();
             $doc->loadXML(file_get_contents($file));
+
             $nodes = $doc->getElementsByTagName('virtualType');
-/** @var \DOMElement $node */
+            /** @var \DOMElement $node */
             foreach ($nodes as $node) {
                 $name = $node->getAttribute('name');
                 $type = $node->getAttribute('type');
